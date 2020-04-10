98a7242fa5df1ad9bde0a3ab2eb37418f053cc79
Maria Guarch <maria@socialandloyal.com>
2020-04-10 10:49:16 +0200

    Remote db

--- servidor/db.js
+++ servidor/db.js
@@ -16,7 +16,7 @@ class Obj {
     // Configura aquí la connexió amb la base de dades
     constructor () {
 
-        this.host           = '127.0.0.1'
+        this.host           = '109.237.25.44'
         this.port           = 3306
         this.db             = 'serverDB'
         this.usr            = 'user'
